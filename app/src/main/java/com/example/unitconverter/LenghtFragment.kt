951bf3bf83024f0e107ee88f99e81f6963af1b28
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2022-08-09 22:47:33 +0200

    New changes to lenght fragment - input, dropdown menus, button.

--- app/src/main/java/com/example/unitconverter/LenghtFragment.kt
+++ app/src/main/java/com/example/unitconverter/LenghtFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.*
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +15,41 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LenghtFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lenght, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LenghtFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LenghtFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val lenght = resources.getStringArray(R.array.lenght_first)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, lenght)
+        getView()?.findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)?.setAdapter(arrayAdapter)
+        getView()?.findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView2)?.setAdapter(arrayAdapter)
+
+        var inputLenghtUser = getView()?.findViewById<EditText>(R.id.editTextNumberDecimal)
+        var calculateButton = getView()?.findViewById<Button>(R.id.button)
+        var resultText: TextView? = getView()?.findViewById(R.id.result)
+
+        if (calculateButton != null) {
+            calculateButton.setOnClickListener(){
+                var inputDouble:Double = inputLenghtUser.text.
             }
+        }
     }
 }
