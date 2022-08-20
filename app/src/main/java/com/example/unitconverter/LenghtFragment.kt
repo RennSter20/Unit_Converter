a34721be3e9934554852447e140505486a17985c
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2022-08-20 19:04:41 +0200

    Basic lenght measure calculations added.

--- app/src/main/java/com/example/unitconverter/LenghtFragment.kt
+++ app/src/main/java/com/example/unitconverter/LenghtFragment.kt
@@ -5,6 +5,8 @@ import android.app.NotificationManager
 import android.content.Context.NOTIFICATION_SERVICE
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.DEBUG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +15,9 @@ import android.widget.AdapterView.OnItemClickListener
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.BuildConfig.DEBUG
 import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
 
 
 /**
@@ -53,6 +57,7 @@ class LenghtFragment : Fragment() {
         var textMeter:TextView? = getView()?.findViewById(R.id.lenght_meter_result)
         var textDeci:TextView? = getView()?.findViewById(R.id.lenght_decimeter_result)
         var textCenti:TextView? = getView()?.findViewById(R.id.lenght_centimeter_result)
+        var textMili:TextView? = getView()?.findViewById(R.id.lenght_milimeter_result)
 
         var selectedItem:String? = null
 
@@ -74,12 +79,18 @@ class LenghtFragment : Fragment() {
         }
 
         fun setText(selectedItem:String):Boolean {
+
+            if(spinner?.text?.toString() == "Input lenght"){
+                return false
+            }
+
             when{
                 selectedItem.equals("Kilometer") -> {
                     textKilo?.text = inputLenghtUser?.text.toString()
                     textMeter?.text = (inputLenghtUser?.text.toString().toDouble() * 1000).toString()
                     textDeci?.text = (inputLenghtUser?.text.toString().toDouble() * 10000).toString()
                     textCenti?.text = (inputLenghtUser?.text.toString().toDouble() * 100000).toString()
+                    textMili?.text = (inputLenghtUser?.text.toString().toDouble() * 1000000).toString()
                 }
 
                 selectedItem.equals("Meter") -> {
@@ -87,6 +98,31 @@ class LenghtFragment : Fragment() {
                     textMeter?.text = inputLenghtUser?.text.toString()
                     textDeci?.text = (inputLenghtUser?.text.toString().toDouble() * 10).toString()
                     textCenti?.text = (inputLenghtUser?.text.toString().toDouble() * 100).toString()
+                    textMili?.text = (inputLenghtUser?.text.toString().toDouble() * 1000).toString()
+                }
+
+                selectedItem.equals("Decimeter") -> {
+                    textKilo?.text = ((inputLenghtUser?.text.toString().toDouble() * 0.0001).toString())
+                    textMeter?.text = (inputLenghtUser?.text.toString().toDouble() * 0.1).toString()
+                    textDeci?.text = inputLenghtUser?.text.toString()
+                    textCenti?.text = (inputLenghtUser?.text.toString().toDouble() * 10).toString()
+                    textMili?.text = (inputLenghtUser?.text.toString().toDouble() * 100).toString()
+                }
+
+                selectedItem.equals("Centimeter") -> {
+                    textKilo?.text = ((inputLenghtUser?.text.toString().toDouble() * 0.00001).toString())
+                    textMeter?.text = (inputLenghtUser?.text.toString().toDouble() * 0.01).toString()
+                    textDeci?.text = (inputLenghtUser?.text.toString().toDouble() * 0.1).toString()
+                    textCenti?.text = inputLenghtUser?.text.toString()
+                    textMili?.text = (inputLenghtUser?.text.toString().toDouble() * 10).toString()
+                }
+
+                selectedItem.equals("Milimeter") -> {
+                    textKilo?.text = ((inputLenghtUser?.text.toString().toDouble() * 0.000001).toString())
+                    textMeter?.text = (inputLenghtUser?.text.toString().toDouble() * 0.001).toString()
+                    textDeci?.text = (inputLenghtUser?.text.toString().toDouble() * 0.01).toString()
+                    textCenti?.text = (inputLenghtUser?.text.toString().toDouble() * 0.1).toString()
+                    textMili?.text = inputLenghtUser?.text.toString()
                 }
             }
             return true
@@ -99,9 +135,11 @@ class LenghtFragment : Fragment() {
                         var view: View? = activity?.findViewById(android.R.id.content)
                         Snackbar.make(view!!, "Please enter lenght.", Snackbar.LENGTH_SHORT).show()
                     }else{
-
+                            Log.i("SPINNER TEXT", spinner?.text.toString())
                             if(setText(selectedItem.toString())){
                                 Snackbar.make(requireView(), "Your lenght has been calculated!", Snackbar.LENGTH_SHORT).show()
+                            }else{
+                                Snackbar.make(requireView(), "Please select lenght measure!", Snackbar.LENGTH_SHORT).show()
                             }
 
                     }
